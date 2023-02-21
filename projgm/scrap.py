dafed5b20a219ddff05dcff2ecaf71020a1256b9
Dorian <gaspardori@cy-tech.fr>
2023-02-21 15:45:50 +0100

    optimizing

--- projgm/scrap.py
+++ projgm/scrap.py
@@ -1,30 +1,25 @@
-from selenium import webdriver
 from multiprocessing import Pool, cpu_count
 from math import floor
+import requests
 
 import pandas as pd
 from bs4 import BeautifulSoup
 from time import *
 #61:76
 #composition lignes : 0-15 infos + 2 details
-starttime = time()
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
 
 url = "https://ec.europa.eu/clima/ets/transaction.do;EUROPA_EUTLPUBLI001_PRD_JSESSIONID=C1IgnSrb9RcUm5vE6ZA66pQQc8bl7zrqNocLBdUJAFZXZ0Ni__Kj!-1038548499?languageCode=fr&startDate=&endDate=&transactionStatus=4&fromCompletionDate=&toCompletionDate=&transactionID=&transactionType=-1&suppTransactionType=-1&originatingRegistry=-1&destinationRegistry=-1&originatingAccountType=-1&destinationAccountType=-1&originatingAccountIdentifier=&destinationAccountIdentifier=&originatingAccountHolder=&destinationAccountHolder=&currentSortSettings=&nextList=Next%3E&resultList.currentPageNumber="
-dictlist = []
 
-pageCount = 100
+limit = 100
 
 def getPageData(n):
     dictlistTmp = []
-    driver = webdriver.Chrome(executable_path="chromedriver", options = options)
-    driver.get(url + str(n))
-    data = driver.page_source
-
-    print("Analyse de la page {}".format(n+1))
-        
-    soup = BeautifulSoup(data,"html.parser")
+    try:
+        data = requests.get(url + str(n))
+    except:
+        print(f"error on page {n}")
+        return dictlistTmp
+    soup = BeautifulSoup(data.text,"html.parser")
     tables = soup.findChildren('table')[7]
     lines = tables.findChildren(['td'])
     counterlinepage = 0
@@ -34,29 +29,25 @@ def getPageData(n):
 
     return dictlistTmp
 
-def getsoup(dictlist):
+def getsoup(dictlist, startPage, endPage):
     global dataframe
     pool = Pool()
 
     ITERATION_COUNT = cpu_count()-1 #-1 pour ne pas faire douiller l'ordinateur
 
-    count_per_iteration = floor(pageCount/int(ITERATION_COUNT))
+    count_per_iteration = floor((endPage-startPage)/int(ITERATION_COUNT))
 
     for i in range(0, ITERATION_COUNT-1):
-        for res in pool.map(getPageData, [n for n in range(count_per_iteration*i, count_per_iteration*(i+1))]):
-            dictlist.extend(res)
-    for res in pool.map(getPageData, [n for n in range(count_per_iteration*(ITERATION_COUNT-1), pageCount)]):
-        dictlist.extend(res)
+        for res in pool.map(getPageData, [n for n in range(count_per_iteration*i+startPage, count_per_iteration*(i+1)+startPage)]):
+            if res != []: dictlist.extend(res)
+    for res in pool.map(getPageData, [n for n in range(count_per_iteration*(ITERATION_COUNT-1)+startPage, endPage)]):
+        if res != []: dictlist.extend(res)
     
     pool.close()
     pool.join()        
     
     dataframe = pd.DataFrame(dictlist)
-    endtime = time()
-    exectime = endtime - starttime
-    print(dataframe)
-    print("{} pages in {}s".format(pageCount, round(exectime)))
-    export()
+    export(f"{startPage}-{endPage}")
 
 def getsoupdata(counterlinepage, lines, dictlist):
     dict = {}
@@ -83,8 +74,15 @@ def getsoupdata(counterlinepage, lines, dictlist):
     
     dictlist.append(dict)
 
-def export():
-    dataframe.to_csv("results.csv")
+def export(name):
+    dataframe.to_csv(name + ".csv")
 
 if __name__ == "__main__":
-    getsoup(dictlist)
+    starttime = time()
+    for i in range(0, limit, 100):
+        dictlist = []
+        print(f"processing batch {i} to {i+100}")
+        getsoup(dictlist, i, i+100)
+    endtime = time()
+    exectime = endtime - starttime
+    print(f"{limit} pages done in {exectime//60} mn et {exectime%60} s !")
