c9f7dbeb2667d9073ec377bb13fdc81bb6d19e10
dogasp <dorian.gaspar@orange.fr>
2023-04-15 01:55:19 +0200

    cleaned code

--- projgm/scrap.py
+++ projgm/scrap.py
@@ -5,15 +5,21 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from time import *
-#61:76
-#composition lignes : 0-15 infos + 2 details
+# 61:76
+# composition lignes : 0-15 infos + 2 details
 
+# url de la page à scrapper
 url = "https://ec.europa.eu/clima/ets/transaction.do;EUROPA_EUTLPUBLI001_PRD_JSESSIONID=C1IgnSrb9RcUm5vE6ZA66pQQc8bl7zrqNocLBdUJAFZXZ0Ni__Kj!-1038548499?languageCode=fr&startDate=&endDate=&transactionStatus=4&fromCompletionDate=&toCompletionDate=&transactionID=&transactionType=-1&suppTransactionType=-1&originatingRegistry=-1&destinationRegistry=-1&originatingAccountType=-1&destinationAccountType=-1&originatingAccountIdentifier=&destinationAccountIdentifier=&originatingAccountHolder=&destinationAccountHolder=&currentSortSettings=&nextList=Next%3E&resultList.currentPageNumber="
+
+# spécification de l'intervalle des pages à scrapper
 lower = 2900
 limit = 3000
 
-def getPageData(n):
+
+def getPageData(n: int) -> list:
+    """Fonction utilisée pour récupérer le contenu de chaque page de la recherche"""
     dictlistTmp = []
+    # boucle pour s'assurer qu'on récupère bien la page
     while True:
         try:
             data = requests.get(url + str(n))
@@ -25,43 +31,53 @@ def getPageData(n):
         except:
             print(f"error on page {n}")
             sleep(1)
-    soup = BeautifulSoup(data.text,"html.parser")
+    # extraction des lignes de la page
+    soup = BeautifulSoup(data.text, "html.parser")
     tables = soup.findChildren('table')[7]
     lines = tables.findChildren(['td'])
-    counterlinepage = 0
-    while counterlinepage < 20:
-        getsoupdata(counterlinepage, lines, dictlistTmp)
-        counterlinepage += 1
+    # pour chaque ligne, on ajoute ses données à la liste
+    for i in range(20):
+        dictlistTmp = getsoupdata(i, lines, dictlistTmp)
 
     return dictlistTmp
 
-def getsoup(dictlist, startPage, endPage):
-    global dataframe
+
+def getsoup(dictlist: list, startPage: int, endPage: int):
+    """Fonction qui réparti la charge de travail entre chaque thread et qui enregistre le résultat"""
     pool = Pool()
 
-    ITERATION_COUNT = cpu_count()-1 #-1 pour ne pas faire douiller l'ordinateur
+    # Nombre de threads :-1 pour ne pas faire douiller l'ordinateur
+    THREAD_COUNT = cpu_count()-1
 
-    count_per_iteration = floor((endPage-startPage)/int(ITERATION_COUNT))
+    # nombre de pages à traiter par thread
+    count_per_iteration = floor((endPage-startPage)/int(THREAD_COUNT))
 
-    for i in range(0, ITERATION_COUNT-1):
+    # répartition de la charge de travail suivant chaque thread
+    for i in range(0, THREAD_COUNT-1):
         for res in pool.map(getPageData, [n for n in range(count_per_iteration*i+startPage, count_per_iteration*(i+1)+startPage)]):
-            if res != []: dictlist.extend(res)
-    for res in pool.map(getPageData, [n for n in range(count_per_iteration*(ITERATION_COUNT-1)+startPage, endPage)]):
-        if res != []: dictlist.extend(res)
-    
-    pool.close()
-    pool.join()        
-    
-    dataframe = pd.DataFrame(dictlist)
-    export(f"{startPage}-{endPage}")
+            if res != []:
+                dictlist.extend(res)
+    # le dernier thread prend le "reste" car le nombre de pages à traiter n'est pas forcément proportionnel au nombre de threads
+    for res in pool.map(getPageData, [n for n in range(count_per_iteration*(THREAD_COUNT-1)+startPage, endPage)]):
+        if res != []:
+            dictlist.extend(res)
 
-def getsoupdata(counterlinepage, lines, dictlist):
+    pool.close()
+    pool.join()
+
+    # sauvegarde des données dans un fichier csv
+    dataframe = pd.DataFrame(dictlist)
+    dataframe.to_csv(f"{startPage}-{endPage}.csv")
+
+
+def getsoupdata(counterlinepage: int, lines: list, dictlist: list) -> list:
+    """Fonction qui sauvegarde les donées d'une ligne"""
     dict = {}
-    table=[]
-    
+    table = []
+
     for line in lines[counterlinepage*17 + 61:counterlinepage*17 + 61 + 15]:
-        table.append(line.get_text(" ",strip=True))
-        
+        table.append(line.get_text(" ", strip=True))
+
     dict["transaction_ID"] = table[0]
     dict["transaction_type"] = table[1]
     dict["transaction_date"] = table[2]
@@ -77,18 +93,18 @@ def getsoupdata(counterlinepage, lines, dictlist):
     dict["acquiring_account_ID"] = table[12]
     dict["acquiring_account_holder"] = table[13]
     dict["units"] = table[14]
-    
-    dictlist.append(dict)
 
-def export(name):
-    dataframe.to_csv(name + ".csv")
+    dictlist.append(dict)
+    return dictlist
+
 
 if __name__ == "__main__":
     starttime = time()
+    # découpage de la charge de travail en batch de 100
     for i in range(lower, limit, 100):
         dictlist = []
         print(f"processing batch {i} to {i+100}")
         getsoup(dictlist, i, i+100)
     endtime = time()
     exectime = endtime - starttime
-    print(f"{limit} pages done in {exectime//60} mn et {exectime%60} s !")
+    print(f"{limit-lower} pages done in {exectime//60} mn et {exectime%60} s !")
--- projgm/scrappage.py
+++ projgm/scrappage.py
@@ -8,203 +8,228 @@ from http.client import RemoteDisconnected
 from time import *
 import math
 from csv import writer
+
 url = "https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=en&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&nextList=Next%3E&resultList.currentPageNumber="
 fromPage = 1
 toPage = 897
+
 links = []
-totalcrashes=0
-totallinks=0
+totalcrashes = 0
+totallinks = 0
 done = []
-CH_countries=["Belgium","Croatia","Finland","France","Allemagne","Greece","Hungary","Iceland","Ireland","Italy","Latvia","Lituania","Malta","Netherlands","Poland","Portugal","Romania","Slovakia","Sweden","United Kingdom"]
+CH_countries = ["Belgium", "Croatia", "Finland", "France", "Allemagne", "Greece", "Hungary", "Iceland", "Ireland", "Italy",
+                "Latvia", "Lituania", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Sweden", "United Kingdom"]
 
-def write(op_info,compliance,CH):
-    op_info.append(compliance)
-    op_info.append(CH)
-    with open("results2.csv",mode="a+",newline='') as csv:
-        writer(csv,delimiter=",").writerow(op_info)
 
-#ligne 54
-def getpage(link, isAirline,is_CH):
+def write(op_info: list, compliance: list, CH: list):
+    """Fonction pour écrire le résultat dans le csv du résultat"""
+    op_info.append(compliance if compliance is not None else [])
+    op_info.append(CH if CH is not None else [])
+    with open("results2.csv", mode="a+", newline='') as csv:
+        writer(csv, delimiter=",").writerow(op_info)
+
+
+def getpage(link: str, isAirline: bool, is_CH: bool):
+    """Fonction permettant de récupérer le contenu d'une page Holding"""
+    # check si la page n'as pas déjà été scrappée
     if link in done:
         return
+    # code permettant d'essayer en boucle jusqu'à ce que la page soit récupérée
     while True:
-        data=requests.get(link)
-        if data.status_code == 200:
-            break
-        else:
-            sleep(1)
+        try:
+            data = requests.get(link)
+            if data.status_code == 200:
+                break
+            else:
+                sleep(1)
+        except:
+            print("error")
 
-    done.append(link)    
-    soup = BeautifulSoup(data.text,"html.parser")
+    done.append(link)
+    # récupération du contenu de la page
+    soup = BeautifulSoup(data.text, "html.parser")
     op_info = operator_info(soup, link)
-    compliance = compliance_info(soup,isAirline) #ne fonctionne que pour les operateurs aériens
+    compliance = compliance_info(soup, isAirline)
     if is_CH and isAirline:
-        CH=CH_scrap(soup)
+        CH = CH_scrap(soup)
     else:
-        CH=""
-    write(op_info,compliance,CH)
-    
-def compliance_info(soup,is_airline):
+        CH = ""
+    write(op_info if op_info is not None else [], compliance, CH)
+
+
+def compliance_info(soup: BeautifulSoup, is_airline: bool) -> str:
+    """Fonction permettant de récupérer les données de la Holding suivant si c'est une airline ou non"""
     if is_airline:
         tables = soup.findChildren('table')[5]
         lines = tables.findChildren(['td'])[54]
-        df = pd.read_html(lines.findChildren("td")[81].prettify())[0]
-        df.drop(columns=9,axis=1,inplace=True)
-        df.drop(columns=8,axis=1,inplace=True)
-        df.drop(index=0,axis=0,inplace=True)
-        df.drop(index=1,axis=0,inplace=True)
-        df.drop(index=28,axis=0,inplace=True)
-        df.drop(index=29,axis=0,inplace=True)
-        df.drop(index=30,axis=0,inplace=True)
-        df.drop(index=31,axis=0,inplace=True)
-        df.drop(index=32,axis=0,inplace=True)
-        
+        try:
+            df = pd.read_html(lines.findChildren("td")[81].prettify())[0]
+        except:
+            print("got a problem")
+            return
+        df.drop(columns=9, axis=1, inplace=True)
+        df.drop(columns=8, axis=1, inplace=True)
+        df.drop(index=0, axis=0, inplace=True)
+        df.drop(index=1, axis=0, inplace=True)
+        df.drop(index=28, axis=0, inplace=True)
+        df.drop(index=29, axis=0, inplace=True)
+        df.drop(index=30, axis=0, inplace=True)
+        df.drop(index=31, axis=0, inplace=True)
+        df.drop(index=32, axis=0, inplace=True)
+
     else:
         tables = soup.findChildren('table')[5]
         lines = tables.findChildren(['td'])[54]
-        df = pd.read_html(lines.findChildren("td")[79].prettify())[0]
-        df.drop(columns=9,axis=1,inplace=True)
-        df.drop(columns=8,axis=1,inplace=True)
-        df.drop(index=0,axis=0,inplace=True)
-        df.drop(index=1,axis=0,inplace=True)
-        df.drop(index=28,axis=0,inplace=True)
-        df.drop(index=29,axis=0,inplace=True)
-        df.drop(index=30,axis=0,inplace=True)
-        df.drop(index=31,axis=0,inplace=True)
-        df.drop(index=32,axis=0,inplace=True)
-        df.drop(index=33,axis=0,inplace=True)
-        
-    total=[]
-    totall=[]
-    totalll=[]
-    for i in range(2,28):
-        
-        total.append((df.loc[i, :].values.flatten().tolist()))
+        try:
+            df = pd.read_html(lines.findChildren("td")[79].prettify())[0]
+        except:
+            print("got a problem")
+            return
+        df.drop(columns=9, axis=1, inplace=True)
+        df.drop(columns=8, axis=1, inplace=True)
+        df.drop(index=0, axis=0, inplace=True)
+        df.drop(index=1, axis=0, inplace=True)
+        df.drop(index=28, axis=0, inplace=True)
+        df.drop(index=29, axis=0, inplace=True)
+        df.drop(index=30, axis=0, inplace=True)
+        df.drop(index=31, axis=0, inplace=True)
+        df.drop(index=32, axis=0, inplace=True)
+        df.drop(index=33, axis=0, inplace=True)
 
- 
+    total = []
+    totall = []
+    totalll = []
+    for i in range(2, 28):
+        total.append((df.loc[i, :].values.flatten().tolist()))
 
     for row in total:
         totall.append([str(element) for element in row])
-    
 
     for row in totall:
         totalll.append(";".join(row))
-    
-    
-    
+
     return "/".join(totalll)
-     
-def CH_scrap(soup):
+
+
+def CH_scrap(soup: BeautifulSoup) -> str:
+    """Récupération spécifique pour certains pays car structure de la page différente"""
     tables = soup.findChildren('table')[5]
-    df=pd.read_html(tables.findChildren(["td"])[56].prettify())[8]
-    df.drop(index=28,axis=0,inplace=True)
-    df.drop(index=29,axis=0,inplace=True)
-    df.drop(index=30,axis=0,inplace=True)
-    df.drop(index=31,axis=0,inplace=True)
-    df.drop(index=32,axis=0,inplace=True)
-    df.drop(index=0,axis=0,inplace=True)
-    df.drop(index=1,axis=0,inplace=True)
-    df.drop(columns=9,axis=1,inplace=True)
-    df.drop(columns=8,axis=1,inplace=True)
-    total=[]
-    totall=[]
-    totalll=[]
-    for i in range(2,28):
-        
-        total.append((df.loc[i, :].values.flatten().tolist()))
+    try:
+        df = pd.read_html(tables.findChildren(["td"])[56].prettify())[8]
+    except:
+        print("got a problem")
+        return
+    df.drop(index=28, axis=0, inplace=True)
+    df.drop(index=29, axis=0, inplace=True)
+    df.drop(index=30, axis=0, inplace=True)
+    df.drop(index=31, axis=0, inplace=True)
+    df.drop(index=32, axis=0, inplace=True)
+    df.drop(index=0, axis=0, inplace=True)
+    df.drop(index=1, axis=0, inplace=True)
+    df.drop(columns=9, axis=1, inplace=True)
+    df.drop(columns=8, axis=1, inplace=True)
 
- 
+    total = []
+    totall = []
+    totalll = []
+
+    for i in range(2, 28):
+        total.append((df.loc[i, :].values.flatten().tolist()))
 
     for row in total:
         totall.append([str(element) for element in row])
-    
 
     for row in totall:
         totalll.append(";".join(row))
     return "/".join(totalll)
 
-def operator_info(soup, link):
-    
+
+def operator_info(soup: BeautifulSoup) -> list:
+
     tables = soup.findChildren('table')[5]
     lines = tables.findChildren(['td'])[54]
     dfs = pd.read_html(lines.findChildren("td")[2].prettify())
     holding_infos = dfs[0]
-    holding_infos.drop(index=0,axis=1,inplace=True)
+    holding_infos.drop(index=0, axis=1, inplace=True)
     holding_infos.reset_index(drop=True, inplace=True)
-    holding_infos.drop(columns=6,inplace=True)
+    holding_infos.drop(columns=6, inplace=True)
     contact_info = dfs[2]
-    contact_info.drop(index=0,axis=1,inplace=True)
-    contact_info.drop(index=1,axis=1,inplace=True)
+    contact_info.drop(index=0, axis=1, inplace=True)
+    contact_info.drop(index=1, axis=1, inplace=True)
     contact_info.reset_index(drop=True, inplace=True)
-    try:
 
+    try:
         dfs = pd.read_html(lines.findChildren("td")[40].prettify())
     except:
-        print(link)
+        print("got a problem")
         return
     operator_infos = dfs[0]
-    operator_infos.drop(index=0,axis=1,inplace=True)
-    operator_infos.drop(columns=10,inplace=True)
+    operator_infos.drop(index=0, axis=1, inplace=True)
+    operator_infos.drop(columns=10, inplace=True)
     adress = dfs[1]
-    adress.drop(index=0,axis=1,inplace=True)
-    adress.drop(columns=10,inplace=True)
-    adress.drop(columns=9,inplace=True)
-    adress.drop(columns=8,inplace=True)
+    adress.drop(index=0, axis=1, inplace=True)
+    adress.drop(columns=10, inplace=True)
+    adress.drop(columns=9, inplace=True)
+    adress.drop(columns=8, inplace=True)
 
     concat_1 = pd.concat([holding_infos, contact_info], axis=1)
-    concat_2 = pd.concat([operator_infos,adress],axis=1)
-    concat_2.reset_index(drop=True,inplace=True)
-    concat_final = pd.concat([concat_1,concat_2],axis=1)
+    concat_2 = pd.concat([operator_infos, adress], axis=1)
+    concat_2.reset_index(drop=True, inplace=True)
+    concat_final = pd.concat([concat_1, concat_2], axis=1)
     row_list = concat_final.loc[1, :].values.flatten().tolist()
 
     return row_list
-    
 
-def is_in(liste, string):
+
+def is_in(liste: list, string: str) -> bool:
+    """Check if string is in one of the element of the list"""
     for e in liste:
         if e in string:
             return True
-        else:
-            return False
+    return False
 
-def processPage(i):
+
+def processPage(i: int):
+    """process each page to be done"""
     global totallinks, totalcrashes
-    try:
-        print("page {}".format(i))
-        while True:
+    while True:
+        try:
             data = requests.get(url + str(i))
             if data.status_code == 200:
                 break
             else:
                 sleep(1)
-        soup = BeautifulSoup(data.text,"html.parser")
-        button = soup.find_all('a', {'class': 'listlink'})
-        for b in button:
-            if "Details - All Phases" in b.text:
-                tmp = b.parent.parent.parent.parent.parent
-                
-                links.append((b['href'], "Aircraft operator activities" in tmp.text, is_in(CH_countries,tmp.text)))
-            
-        for link, isAirline,is_CH in links[0:len(links)]:
-            totallinks+=1
-            
-            getpage(link, isAirline,is_CH)
-    
-    except ConnectionError:
-        totalcrashes+=1
-        print("connection error, total crashes = {}".format(totalcrashes))
+        except:
+            totalcrashes += 1
+            print("connection error, total crashes = {}".format(totalcrashes))
+    soup = BeautifulSoup(data.text, "html.parser")
+    button = soup.find_all('a', {'class': 'listlink'})
+    # find the link associated to the button
+    for b in button:
+        if "Details - All Phases" in b.text:
+            tmp = b.parent.parent.parent.parent.parent
+
+            links.append((b['href'], "Aircraft operator activities" in tmp.text, is_in(
+                CH_countries, tmp.text)))
+
+    for link, isAirline, is_CH in links[0:len(links)]:
+        totallinks += 1
+
+        getpage(link, isAirline, is_CH)
+
 
 if __name__ == "__main__":
     pool = Pool()
-    ITERATION_COUNT = cpu_count()-1 #-1 pour ne pas faire douiller l'ordinateur
+    # save as in scrap.py
+    ITERATION_COUNT = cpu_count()-1
 
     count_per_iteration = floor((toPage-fromPage)/int(ITERATION_COUNT))
 
     for i in range(0, ITERATION_COUNT-1):
-        pool.map(processPage, [n for n in range(count_per_iteration*i+fromPage, count_per_iteration*(i+1)+fromPage)])
-    pool.map(processPage, [n for n in range(count_per_iteration*(ITERATION_COUNT-1)+fromPage, toPage)])
+        pool.map(processPage, [n for n in range(
+            count_per_iteration*i+fromPage, count_per_iteration*(i+1)+fromPage)])
+    pool.map(processPage, [n for n in range(
+        count_per_iteration*(ITERATION_COUNT-1)+fromPage, toPage)])
 
     pool.close()
     pool.join()
-    
-        
